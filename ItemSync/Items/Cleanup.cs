ecfc5bc8c1ab3f4fcda1386176b3a34e91a7a899
Marius <e@mail.com>
2018-04-23 18:31:15 +0200

    Fixed a design flaw in the cleanup method (did not work with the unreliable timers in azure)

--- ItemSync/Items/Cleanup.cs
+++ ItemSync/Items/Cleanup.cs
@@ -1,16 +1,19 @@
 using System;
+using System.Threading.Tasks;
+using ItemSync.Shared.AzureCloudTable;
 using ItemSync.Shared.Model;
 using ItemSync.Shared.Service;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace ItemSync.Items
 {
     public static class Cleanup
     {
         [FunctionName("Cleanup")]
-        public static async void Run(
+        public static async Task Run(
             [TimerTrigger("0 0 3 * * *")]TimerInfo myTimer, 
             TraceWriter log,
             [StorageAccount("StorageConnectionString")] CloudStorageAccount storageAccount
@@ -19,9 +22,32 @@ namespace ItemSync.Items
 
             var client = storageAccount.CreateCloudTableClient();
             new ThrottleService(client, log).Clear();
-            
-            // Not the perfect solution, but unlikely to ever affect anyone.
-            await client.GetTableReference(EmailAuthToken.TableName).DeleteIfExistsAsync();
+
+            CloudTable table = client.GetTableReference(EmailAuthToken.TableName);
+            TableQuery<EmailAuthToken> rangeQuery = new TableQuery<EmailAuthToken>().Where(
+                TableQuery.GenerateFilterConditionForDate("Timestamp", 
+                    QueryComparisons.LessThan, 
+                    DateTime.UtcNow.AddHours(-5)
+                )
+            );
+
+            try {
+                // TODO: Group by PartitionKey and delete in batches.
+                await table.CreateIfNotExistsAsync();
+                var entities = await QueryHelper.ListAll(table, rangeQuery);
+
+                log.Info($"Got {entities.Count} entities for deletion");
+                foreach (var entity in entities) {
+                    await table.ExecuteAsync(TableOperation.Delete(entity));
+                }
+
+            }
+            catch (Exception ex) {
+                log.Warning(ex.Message, ex.Source);
+                throw ex;
+            }
+
+            log.Info("Cleanup completed");
         }
     }
 }
